fb80ed86d349476689f42095adf26316efc428bf
komura-c <affirman03139@outlook.jp>
2023-05-11 08:41:31 +0900

    feat: voice change attempt

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,42 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./@types/window.d";
 import "./App.css";
 import { Overlay } from "./components/Overlay";
-import { scratchStartListen } from "./scripts/scratchStartListen";
+import * as Tone from "tone";
 
 function App() {
   const [isCalledStartListen, setIsCalledStartListen] = useState(false);
+  const [meterValue, setMeterValue] = useState(0);
+  // const [pitchValue, setPitchValue] = useState(1);
+  const pitchValue = 5;
 
-  const startListen = async () => {};
+  const startListen = () => {
+    const meter = new Tone.Meter();
+    const pitchShift = new Tone.PitchShift(pitchValue).toDestination();
+    const mic = new Tone.UserMedia().connect(meter);
+    if (!pitchShift) {
+      return;
+    }
+    mic.connect(pitchShift);
+    mic
+      .open()
+      .then(() => {
+        // promise resolves when input is available
+        console.log("mic open");
+        // print the incoming mic levels in decibels
+        setInterval(() => {
+          if (meter.getValue() !== Infinity) {
+            setMeterValue(
+              500 - Math.floor(Math.abs(meter.getValue() as number) * 10)
+            );
+          }
+        }, 100);
+      })
+      .catch((e) => {
+        // promise is rejected when the user doesn't have or allow mic access
+        console.log("mic not open");
+      });
+  };
 
   const screenClick = () => {
     if (showOverlay) {
@@ -24,7 +53,42 @@ function App() {
   return (
     <div onClick={screenClick}>
       <h1>Web Voice Changer</h1>
-      {showOverlay && <Overlay />}
+      {showOverlay ? (
+        <Overlay />
+      ) : (
+        <div>
+          {/* <label htmlFor="inputSetPitch">ピッチ調整</label>
+          <input
+            id="inputSetPitch"
+            type="number"
+            value={pitchValue}
+            onChange={(e) => setPitchValue(Number(e.target.value))}
+          /> */}
+          <div
+            style={{
+              height: "70vh",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            {Array(10)
+              .fill(1)
+              .map((_, index) => (
+                <div
+                  key={index}
+                  style={{
+                    width: 10,
+                    height: meterValue,
+                    content: "",
+                    backgroundColor: "white",
+                    marginRight: 30,
+                  }}
+                ></div>
+              ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -2,8 +2,7 @@ export function Overlay() {
   return (
     <div className="overlay">
       <div className="overlay-content">
-        <h1>Overlay Content</h1>
-        <p>This is the overlay content.</p>
+        <h3>click here to start</h3>
       </div>
     </div>
   );
